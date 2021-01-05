ac5ce6629fcd07afb4398a64a82be3e1f8b32ec7
Andy謝 <a755466@stdmail.nssh.ntpc.edu.tw>
2021-01-05 23:29:40 +0800

    TOI 1
    TOI 1

---
+++ 755466/flag.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int R, C, fail = 0, num;//�C��
+    cin >> R >> C;
+    int S[R + 1][C + 1];
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            S[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> S[i][j];
+        }
+    }
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            num = S[i][j];
+            if (num != S[i - 1][j - 1] && 
+                num != S[i - 1][j] && 
+                num != S[i][j - 1] && 
+                num != S[i + 1][j] && 
+                num != S[i][j + 1] &&
+                num != S[i - 1][j + 1] && 
+                num != S[i + 1][j - 1] &&
+                num != S[i + 1][j + 1]) {
+                fail += 1;
+                }
+            }
+        }
+
+    cout << fail;
+}
